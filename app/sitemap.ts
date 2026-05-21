eea7b98daa95fe24d51271da1f97e26ede88ae60
Disha Mulchandani <disha.mulchandani@example.com>
2026-05-22 00:46:14 +0530

    update webiste

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,19 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://learnora-academy.vercel.app',
+      url: "https://learnora-academy.vercel.app",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
-  ]
+
+    {
+      url: "https://learnora-academy.vercel.app/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
 }
